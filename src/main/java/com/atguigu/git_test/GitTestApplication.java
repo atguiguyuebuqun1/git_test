762909fb9bc480dd11adb7146ad96bb8a1d1e8d8
chenchao <2983722624@qq.com>
2023-01-31 15:12:18 +0800

    four commit

--- src/main/java/com/atguigu/git_test/GitTestApplication.java
+++ src/main/java/com/atguigu/git_test/GitTestApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitTestApplication {
 
     public static void main(String[] args) {
-        System.out.println("spring");
-        System.out.println("spring2");
         SpringApplication.run(GitTestApplication.class, args);
     }
 
