982be42eb22aeb4be3678dd4e348f080e0ae06f9
chenchao <2983722624@qq.com>
2023-02-01 10:40:41 +0800

    hot-fix after1 commit

--- src/main/java/com/atguigu/git_test/GitTestApplication.java
+++ src/main/java/com/atguigu/git_test/GitTestApplication.java
@@ -9,6 +9,7 @@ public class GitTestApplication {
     public static void main(String[] args) {
         System.out.println("");
         System.out.println("hot-fix");
+        System.out.println("hot-fix");
         SpringApplication.run(GitTestApplication.class, args);
     }
 
