b77f49a80b0f6136999bdd57492f920389d539c2
chenchao <2983722624@qq.com>
2023-02-01 10:41:06 +0800

    master after1 commit

--- src/main/java/com/atguigu/git_test/GitTestApplication.java
+++ src/main/java/com/atguigu/git_test/GitTestApplication.java
@@ -9,6 +9,7 @@ public class GitTestApplication {
     public static void main(String[] args) {
         System.out.println("");
         System.out.println("master");
+        System.out.println("master");
         SpringApplication.run(GitTestApplication.class, args);
     }
 
